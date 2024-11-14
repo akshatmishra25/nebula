0c563a465ffe48a89307f5ed696433c7193db00d
Akshat Mishra <akshatmishra317@gmail.com>
2024-11-14 10:34:54 +0530

    added config files

---
+++ config/config.go
@@ -0,0 +1,32 @@
+// config/config.go
+package config
+
+import (
+    "log"
+    "os"
+
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    DBHost     string
+    DBUser     string
+    DBPassword string
+    DBName     string
+    JWTSecret  string
+}
+
+func LoadConfig() Config {
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+
+    return Config{
+        DBHost:     os.Getenv("DB_HOST"),
+        DBUser:     os.Getenv("DB_USER"),
+        DBPassword: os.Getenv("DB_PASSWORD"),
+        DBName:     os.Getenv("DB_NAME"),
+        JWTSecret:  os.Getenv("JWT_SECRET"),
+    }
+}
---
+++ internal/utils/database.go
@@ -0,0 +1,29 @@
+// internal/utils/database.go
+package utils
+
+import (
+    "fmt"
+    "log"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "nebula/config"
+)
+
+var DB *sql.DB
+
+func ConnectDB(cfg config.Config) {
+    psqlInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+        cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+
+    var err error
+    DB, err = sql.Open("postgres", psqlInfo)
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
+
+    if err = DB.Ping(); err != nil {
+        log.Fatalf("Failed to ping database: %v", err)
+    }
+
+    log.Println("Database connected successfully!")
+}
