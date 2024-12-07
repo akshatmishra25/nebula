248c676190c119b5b4a03924a93402285007fcfe
Akshat Mishra <akshatmishra317@gmail.com>
2024-12-08 02:43:37 +0530

    feat: added logic to generate connection string and rectified route handlers

--- internal/projects/service.go
+++ internal/projects/service.go
@@ -1,35 +1,40 @@
 package projects
 
 import (
-    "fmt"
-    "nebula.local/internal/models"
-    "nebula.local/internal/utils"
+	"fmt"
+	"nebula.local/internal/models"
+	"nebula.local/internal/utils"
+	"nebula.local/pkg/connection_string"
 )
 
 func AddProject(userID int, name, description, frontend string) (models.Project, error) {
-    // Generate unique connection string
-    connection := fmt.Sprintf("nebula://user-%d/project-%s", userID, name)
+	// Generate a unique connection string
+	connection, err := connection_string.GenerateConnectionString(userID, name)
+	if err != nil {
+		return models.Project{}, fmt.Errorf("error generating connection string: %v", err)
+	}
 
-    // Insert project into database
-    query := `INSERT INTO projects (user_id, name, description, frontend, connection)
+	// Insert the project into the database
+	query := `INSERT INTO projects (user_id, name, description, frontend, connection)
               VALUES ($1, $2, $3, $4, $5) RETURNING id`
 
-    var projectID int
-    err := utils.DB.QueryRow(query, userID, name, description, frontend, connection).Scan(&projectID)
-    if err != nil {
-        return models.Project{}, err
-    }
+	var projectID int
+	err = utils.DB.QueryRow(query, userID, name, description, frontend, connection).Scan(&projectID)
+	if err != nil {
+		return models.Project{}, err
+	}
 
-    // Return the new project
-    return models.Project{
-        ID:          projectID,
-        UserID:      userID,
-        Name:        name,
-        Description: description,
-        Frontend:    frontend,
-        Connection:  connection,
-    }, nil
+	// Return the new project
+	return models.Project{
+		ID:          projectID,
+		UserID:      userID,
+		Name:        name,
+		Description: description,
+		Frontend:    frontend,
+		Connection:  connection,
+	}, nil
 }
+
 
 func GetProjectsByUser(userID int) ([]models.Project, error) {
     query := `SELECT id, user_id, name, description, frontend, connection FROM projects WHERE user_id = $1`
